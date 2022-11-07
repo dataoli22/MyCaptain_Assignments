9c986adeef7feb395ed07dd1cf281dfe98cea44e
dataoli22 <117657044+dataoli22@users.noreply.github.com>
2022-11-07 22:31:50 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,28 @@
+import math
+
+input1= float(input('Input the radius of your circle: '))
+input2= math.pi
+
+output= input2* input1* input1
+
+print(f'The area of the circle is: {output}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
