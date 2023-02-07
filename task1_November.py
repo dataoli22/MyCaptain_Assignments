489e974870b09a5d3b40e4e640a61f78fa8b591a
Olipriya Chakraborty <117657044+dataoli22@users.noreply.github.com>
2023-02-07 21:22:20 +0530

    Update task1_November.py

--- task1_November.py
+++ task1_November.py
@@ -1,3 +1,9 @@
+'''Your task now is to write a Python program which accepts the radius of a circle from the user and computes area.
+Your Input and Output should look something like this
+
+Input the radius of the circle : 1.1 The area of the circle with radius 1.1 is: 3.8013271108436504
+''''
+
 import math
 
 input1= float(input('Input the radius of your circle: '))
