e56bcfbb3381415aed45f4c51bd518322b646e07
Olipriya Chakraborty <117657044+dataoli22@users.noreply.github.com>
2023-02-28 21:29:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+df = pd.read_excel('Dataset.xlsx') 
+
+print(df)
+
+# Generate Report
+profile = ProfileReport(df)
+#profile = ProfileReport(df, minimal=True)
+profile.to_file(output_file="dateset.html")
